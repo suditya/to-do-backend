2cb8e48657a0b3622fa3edcbd9fcb0a688bbc545
suditya <guptasuditya@gmail.com>
2023-06-17 14:34:21 +0530

    corrected connection str

--- data/db.js
+++ data/db.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 
 const {NODE_ENV,DB_HOST,DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 
-const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` :  `mongodb+srv://${DB_USER}:${DB_PASSWORD}$@{DB_HOST}/${DB_NAME}/?retryWrites=true&w=majority`
+// const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` :  `mongodb+srv://${DB_USER}:${DB_PASSWORD}$@{DB_HOST}/${DB_NAME}/?retryWrites=true&w=majority`
+
+const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` :  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}/?retryWrites=true&w=majority`
+
 
 // mongodb+srv://sudityagupta:<password>@todoapp.g4fq29p.mongodb.net/
 // const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` :  `mongodb+srv://${DB_USER}:${DB_PASSWORD}${DB_HOST}/?retryWrites=true&w=majority`
