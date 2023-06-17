ad23da59fa8bd0bb45cb454f3d5dc601db359699
suditya <guptasuditya@gmail.com>
2023-06-17 14:53:37 +0530

    changed some code for connection str

--- data/db.js
+++ data/db.js
@@ -11,8 +11,13 @@ const {NODE_ENV,DB_HOST,DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 // mongodb+srv://sudityagupta:<password>@todoapp.g4fq29p.mongodb.net/
 // const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` :  `mongodb+srv://${DB_USER}:${DB_PASSWORD}${DB_HOST}/?retryWrites=true&w=majority`
 
+// mongodb+srv://sudityagupta:<password>@todoapp.g4fq29p.mongodb.net/?retryWrites=true&w=majority
 
 const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` : `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+if(NODE_ENV !== 'development')
+{
+    connectionStr = 'mongodb+srv://sudityagupta:GzX2zuBDjyf3uaQq@todoapp.g4fq29p.mongodb.net/?retryWrites=true&w=majority'
+}
 
 console.log(connectionStr, "connection str");
 // mongodb+srv://username:password@hostname/database
